0b0cd579b4116829553c4e6cfbccbc02fdfc090d
SauceChord <58859889+SauceChord@users.noreply.github.com>
2020-04-29 22:02:30 +0200

    Change set intersection to & operator

--- practicepython/exercise5.py
+++ practicepython/exercise5.py
@@ -5,7 +5,7 @@ import random
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-print (list(set(a).intersection(b)))
+print (list(set(a) & set(b)))
 
 # Extra 1:
 # Randomly generate two lists to test this
@@ -15,10 +15,10 @@ d = [random.randrange(20) for i in range(15)]
 
 print ("list c {}".format(c))
 print ("list d {}".format(d))
-print ("common between c and d {}".format(list(set(c).intersection(d))))
+print ("common between c and d {}".format(list(set(c) & set(d))))
 
 # Extra 2:
 # Write this in one line of Python (don’t worry if you can’t figure this 
 # out at this point - we’ll get to it soon)
 
-print(list(set([random.randrange(20) for i in range(15)]).intersection([random.randrange(20) for i in range(15)])))
+print(list(set([random.randrange(20) for i in range(15)]) & set([random.randrange(20) for i in range(15)])))
