70dd9e0cd8d3c495db1ea34831e5834909fbbdc7
SauceChord <58859889+SauceChord@users.noreply.github.com>
2020-04-29 21:58:18 +0200

    practicepython.org exercise 5

---
+++ practicepython/exercise5.py
@@ -0,0 +1,24 @@
+# https://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+
+import random
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+print (list(set(a).intersection(b)))
+
+# Extra 1:
+# Randomly generate two lists to test this
+
+c = [random.randrange(20) for i in range(15)]
+d = [random.randrange(20) for i in range(15)]
+
+print ("list c {}".format(c))
+print ("list d {}".format(d))
+print ("common between c and d {}".format(list(set(c).intersection(d))))
+
+# Extra 2:
+# Write this in one line of Python (don’t worry if you can’t figure this 
+# out at this point - we’ll get to it soon)
+
+print(list(set([random.randrange(20) for i in range(15)]).intersection([random.randrange(20) for i in range(15)])))
