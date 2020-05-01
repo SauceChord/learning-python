eef70d809a3ed82fc2c9e8ac64253d7e372b7440
SauceChord <58859889+SauceChord@users.noreply.github.com>
2020-05-01 20:38:44 +0200

    add execise instructions

--- book_exercises/r_1_5.py
+++ book_exercises/r_1_5.py
@@ -1,1 +1,4 @@
+# Give a single command that computes the sum from Exercise R-1.4, 
+# relying on Python’s comprehension syntax and the built-in sum function
+
 print(sum(i ** 2 for i in range(1, 4)))
